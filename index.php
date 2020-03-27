d7353082bcae23c9d7a9f1293d7eb27aa5a23494
hsutungyu <55544247+hsutungyu@users.noreply.github.com>
2020-03-27 19:22:16 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <head>
     <link rel="stylesheet" type="text/css" href="/css/style-nav.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Baloo+2&display=swap" rel="stylesheet">
     <title>Trade2CU - Homepage</title>
     <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
 </head>
@@ -11,7 +12,9 @@
 <body>
     <?php
     session_start();
+    include_once('member/register.php');
     ?>
+<div class="container">
     <div class="navbar">
 
 <ul>
@@ -23,7 +26,7 @@
             </ul>
         </li>
         <li><a href="/product/selling.php" class="navbar-text">Selling</a></li>
-        <li><a href="/index.php" class="navbar-img active"><img src="/img/test.png" height="30px" align="middle"></a>
+        <li><a href="/index.php" class="navbar-img active"><img src="/img/logo.png" height="60px" align="middle"></a>
         </li>
         <li><a class="navbar-dropdown2-button">Search</a>
             <ul class="navbar-dropdown2-content">
@@ -44,33 +47,77 @@
             if (!isset($_SESSION['username'])) {
                 ?>
                 <li><a class="navbar-text">Welcome Guest</a>
-                <ul class="navbar-login-content"><li><div class="form"><form action="" method="post"><a>Account:</a><input type="text" id="username" name="username"><a>Password:</a><input type="password" id="password" name="password"><input type="submit" name="login" value="Login"></form></div></li></ul></li>
+                <ul class="navbar-login-content"><li><div class="form"><form action="" method="post"><a>Account:</a><input type="text" id="username" name="username"><a>Password:</a><input type="password" id="password" name="password"><input type="submit" name="login" value="Login"></form></div></li></ul>
+                <ul class="navbar-dropdown1-content"><li><a id="register-modal-button">Register</a></li></ul></li>
                 <?php
             }
-            define('DB_SERVER', 'localhost');
-            define('DB_USERNAME', 'root');
-            define('DB_PASSWORD', '123456');
-            define('DB_NAME', 'project');
+            defined('DB_SERVER') or define('DB_SERVER', 'localhost');
+            defined('DB_USERNAME') or define('DB_USERNAME', 'root');
+            defined('DB_PASSWORD') or define('DB_PASSWORD', '123456');
+            defined('DB_NAME') or define('DB_NAME', 'project');
 
             $link=mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-
-            if (isset($_POST['login'])) {
-                $username=$_POST['username'];
-                $password=$_POST['password'];
-                $query=mysqli_query($link, "select * from member where username='$username' and password='$password'");
-                if (mysqli_num_rows($query)==1) {
-                    $_SESSION['username']=$username;
-                    header("Refresh:0");
-                } else {
-                    echo("Error");
-                }
+            function alert($msg){
+                echo "<script type='text/javascript'>alert('$msg');</script>";
             }
+            if (isset($_POST['login'])) {
+                if (!empty($_POST['username'])) {
+                    $username=$_POST['username'];
+                }
+                if (!empty($_POST['password'])){
+                    $password=$_POST['password'];
+                }
+                if (!empty($username)&&!empty($password)) {
+                    $password_hashed=password_hash($password, PASSWORD_DEFAULT);
+                    $query=mysqli_query($link, "select password from member where username='$username'");
+                    if (mysqli_num_rows($query)==1) {
+                        if (password_verify($password, mysqli_fetch_array($query)['password'])) {
+                            $_SESSION['username']=$username;
+                            header("Refresh:0");
+                        } else 
+                            alert("Username or Password is incorrect!");
+                        }
+                    }else{
+                        alert("Username or Password cannot be empty!");
+                    }
+            }
+            
             ?>
-            <!--<a href="member/information.php" class="navbar-text">Member</a></li>-->
+            
 
     </ul>
 
 </div>
+<br><br><br><br><br><br><br><br>
+<h2>News Feed</h2>
+<?php
+if(!isset($_SESSION['username'])){
+?>
+
+    <p>After logging in, you can view your news feed here.</p>
+    <p>This includes new comments on your products, new messages that you receive and many more!</p>
+
+<?php
+}else{
+    ?>
+    <ul class="news-feed-login">
+    <li><a>abc commented on your product xyz.</a></li>
+    <li><a>your product xyz has been bought. See details.</li>
+    </ul>
+    <?php
+}
+?>
+<br><br><br>
+<h2>Product Suggestion</h2>
+<p>Season of spring! Your dorm may need a dehumidifier. Here are some products that we think you may need:</p>
+        </div>
+
+<footer class="footer">
+    <h4 style="text-align:center">Trade2CU</h4>
+    <a style="text-align:center" href="/aboutus/aboutus.php">About Us</a><br>
+    <a style="text-align:center;margin-bottom:10px;" href="/aboutus/faq.php">FAQ</a><br>
+</footer>
+<script src="/member/register.js"></script>
 </body>
 
 
