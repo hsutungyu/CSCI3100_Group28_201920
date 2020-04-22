eebf01e4e0ada94e8457e2b9d5064961292b2c02
ansonngg <t.kureya1224@gmail.com>
2020-04-23 06:34:34 +0800

    added 3 files

---
+++ template/index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>Trade2CU - Homepage</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+<link href="https://fonts.googleapis.com/css?family=Baloo+2:500&display=swap&subset=latin-ext" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="../css/style-template.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+
+<div class="wrapper">
+
+<div class="navbar">
+	<ul>
+		<li><a class="img" href="../index.php"><img src="../img/logo.png" alt="Trade2CU logo"></img></a></li>
+		<li><a href="../product/selling.php">Sell</a></li>
+		<li><a href="#contact">Category</a></li>
+		<li>
+			<form>
+				<input type="text" placeholder="Search.." name="search">
+				<button type="submit"><i class="fa fa-search"></i></button>
+			</form>
+		</li>
+		<li style="float:right"><a href="#login">Login</a></li>
+		<li style="float:right"><a href="#account">Account</a></li>
+	</ul>
+</div>
+
+<div class="navbar">
+	<ul>
+		<li style="float:right;"><a href="#cart" style="padding:1px;padding-right:16px;">Shopping Cart</a></li>
+		<li style="float:right;"><img src="../img/cart.png" style="height:16px;padding:5px;"></li>
+	</ul>
+</div>
+
+<h2 style="position:relative;left:30px;">New items</h2>
+
+<div class="navbar">
+	<ul>
+		<li><a href="#NA"><img src="../img/product/5e9e24.jpg" style="height:150px;"></img></a></li>
+		<li><a href="#NA"><img src="../img/product/5e9ef4.jpg" style="height:150px;"></img></a></li>
+		<li><a href="#NA"><img src="../img/product/129jiw.jpg" style="height:150px;"></img></a></li>
+		<li><a href="#NA"><img src="../img/product/510923.jpg" style="height:150px;"></img></a></li>
+		<li><a href="#new"><i class="fa fa-angle-double-right"style="font-size:5em;color:#cccccc;position:relative;top:30px;left:10px"></i></a></li>
+	</ul>
+</div>
+
+<br>
+
+<h2 style="position:relative;left:30px;">Things you may like</h2>
+
+<div class="navbar">
+	<ul>
+		<li><a href="#NA"><img src="../img/product/5e9e24.jpg" style="height:150px;"></img></a></li>
+		<li><a href="#NA"><img src="../img/product/5e9ef4.jpg" style="height:150px;"></img></a></li>
+		<li><a href="#NA"><img src="../img/product/129jiw.jpg" style="height:150px;"></img></a></li>
+		<li><a href="#NA"><img src="../img/product/510923.jpg" style="height:150px;"></img></a></li>
+		<li><a href="#favourite"><i class="fa fa-angle-double-right"style="font-size:5em;color:#cccccc;position:relative;top:30px;left:10px"></i></a></li>
+	</ul>
+</div>
+
+<br>
+
+<h2 style="position:relative;left:30px;">Categories</h2>
+
+<div class="navbar">
+	<ul>
+		<li><a href="#NA"><img src="../img/product/5e9e24.jpg" style="height:150px;"></img></a></li>
+		<li><a href="#NA"><img src="../img/product/5e9ef4.jpg" style="height:150px;"></img></a></li>
+		<li><a href="#NA"><img src="../img/product/129jiw.jpg" style="height:150px;"></img></a></li>
+		<li><a href="#NA"><img src="../img/product/510923.jpg" style="height:150px;"></img></a></li>
+		<li><a href="#category"><i class="fa fa-angle-double-right"style="font-size:5em;color:#cccccc;position:relative;top:30px;left:10px"></i></a></li>
+	</ul>
+</div>
+
+<div class="push"></div>
+
+</div>
+
+<div class="navbar">
+	<ul>
+		<li><a class="img" href="../index.php"><img src="../img/logo.png" alt="Trade2CU logo"></img></a></li>
+		<li><a href="../aboutus/aboutus.php">About Us</a></li>
+		<li><a href="../aboutus/faq.php">FAQ</a></li>
+	</ul>
+</div>
+
+</body>
+
+</html>
---
+++ template/information.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>Trade2CU - User Information</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+<link href="https://fonts.googleapis.com/css?family=Baloo+2:500&display=swap&subset=latin-ext" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="../css/style-template.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+
+<div class="wrapper">
+
+<div class="navbar">
+	<ul>
+		<li><a class="img" href="../index.php"><img src="../img/logo.png" alt="Trade2CU logo"></img></a></li>
+		<li><a href="../product/selling.php">Sell</a></li>
+		<li><a href="#contact">Category</a></li>
+		<li>
+			<form>
+				<input type="text" placeholder="Search.." name="search">
+				<button type="submit"><i class="fa fa-search"></i></button>
+			</form>
+		</li>
+		<li style="float:right"><a href="#login">Login</a></li>
+		<li style="float:right"><a href="#account">Account</a></li>
+	</ul>
+</div>
+
+<div class="navbar">
+	<ul>
+		<li style="float:right;"><a href="#cart" style="padding:1px;padding-right:16px;">Shopping Cart</a></li>
+		<li style="float:right;"><img src="../img/cart.png" style="height:16px;padding:5px;"></li>
+	</ul>
+</div>
+
+<img src="../img/member/placeholder.png" style="width:160px;" align="left"></img>
+
+<h2 style="position:relative;left:50px;margin-bottom:-10px;margin-top:0px;">Username</h2>
+
+<p style="position:relative;left:50px;margin-bottom:10px">
+	Email: 1155012345@link.cuhk.edu.hk <br>
+	Telephone: <br>
+	FPS payment method:
+</p>
+
+<a href="#edit" style="position:relative;left:50px;">Edit profile</a>
+
+<a href="#change" style="padding-left:50pt;">Change password</a>
+
+<br clear="left">
+
+<button class="button">Edit icon</button>
+
+<p><span style="position:relative;left:30px;font-size:1.5em;font-weight:bold;margin:0.83em 0;padding-right:50px;">Things I am selling</span> <a href="../product/selling.php">sell a new one</a></p>
+
+<div class="navbar">
+	<ul>
+		<li><a href="#NA"><img src="../img/product/5e9e24.jpg" style="height:150px;"></img></a></li>
+		<li><a href="#NA"><img src="../img/product/5e9ef4.jpg" style="height:150px;"></img></a></li>
+		<li><a href="#NA"><img src="../img/product/129jiw.jpg" style="height:150px;"></img></a></li>
+		<li><a href="#NA"><img src="../img/product/510923.jpg" style="height:150px;"></img></a></li>
+		<li><a href="#NA"><img src="../img/product/aoiasd.jpg" style="height:150px;"></img></a></li>
+		<li><a href="#NA"><img src="../img/product/5e9e24.jpg" style="height:150px;"></img></a></li>
+		<li><a href="#NA"><img src="../img/product/5e9ef4.jpg" style="height:150px;"></img></a></li>
+		<li><a href="#NA"><img src="../img/product/129jiw.jpg" style="height:150px;"></img></a></li>
+		<li><a href="#NA"><img src="../img/product/510923.jpg" style="height:150px;"></img></a></li>
+		<li><a href="#NA"><img src="../img/product/aoiasd.jpg" style="height:150px;"></img></a></li>
+		<li><a href="#NA"><img src="../img/product/5e9e24.jpg" style="height:150px;"></img></a></li>
+		<li><a href="#NA"><img src="../img/product/5e9ef4.jpg" style="height:150px;"></img></a></li>
+		<li><a href="#NA"><img src="../img/product/129jiw.jpg" style="height:150px;"></img></a></li>
+		<li><a href="#NA"><img src="../img/product/510923.jpg" style="height:150px;"></img></a></li>
+		<li><a href="#NA"><img src="../img/product/aoiasd.jpg" style="height:150px;"></img></a></li>
+	</ul>
+</div>
+
+<div class="push"></div>
+
+</div>
+
+<div class="navbar">
+	<ul>
+		<li><a class="img" href="../index.php"><img src="../img/logo.png" alt="Trade2CU logo"></img></a></li>
+		<li><a href="../aboutus/aboutus.php">About Us</a></li>
+		<li><a href="../aboutus/faq.php">FAQ</a></li>
+	</ul>
+</div>
+
+</body>
+
+</html>
