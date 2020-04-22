cec3ec039f961446e9603b0994817faf1223b4fa
hsutungyu <55544247+hsutungyu@users.noreply.github.com>
2020-04-23 02:08:08 +0800

    register system v1

--- member/register.js
+++ member/register.js
@@ -2,27 +2,27 @@ var modal = document.getElementById("register-modal");
 var btn = document.getElementById("register-modal-button");
 var span = document.getElementsByClassName("close")[0];
 
-btn.onclick = function() {
+btn.onclick = function () {
   modal.style.display = "block";
 };
 
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
 };
 
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 };
 
-$("#register-form").on("submit", function(event) {
+$("#register-form").on("submit", function (event) {
   event.preventDefault();
   $.ajax({
     url: "/member/registerprocess.php",
     type: "POST",
     data: $("#register-form").serialize(),
-    success: function(data) {
+    success: function (data) {
       var dataObj = JSON.parse(data);
       if (dataObj["usernameErr"] != "") {
         $("#usernameErr").html(dataObj["usernameErr"]);
@@ -39,6 +39,6 @@ $("#register-form").on("submit", function(event) {
       if (dataObj["canSubmit"] == 1) {
         window.location.reload();
       }
-    }
+    },
   });
 });
