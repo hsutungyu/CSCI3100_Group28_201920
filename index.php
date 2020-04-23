ae9da11f8d125c5d298f98146e719b35bc4867e2
RodiseC888 <62474567+RodiseC888@users.noreply.github.com>
2020-04-23 16:52:56 +0800

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,26 @@
     <title>Trade2CU - Homepage</title>
     <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
 </head>
+<style>
+.column {
+  float: left;
+  width: 15%;
+  padding: 15px;
+}
+
+.row::after {
+  content: "";
+  clear: both;
+}
+
+.footer {
+  position: float;
+  bottom: 0;
+  width: 100%;
+  clear:both;
+}
+
+</style>
 
 <body>
     <?php
@@ -135,7 +155,185 @@ if(isset($_SESSION["username"])){
 ?>
 <br><br><br>
 <h2>Product Suggestion</h2>
-<p>Season of spring! Your dorm may need a dehumidifier. Here are some products that we think you may need:</p>
+<?php 
+$servername="localhost";
+$username="root";
+$password="123456";
+$dbname="project";
+$conn=new mysqli($servername, $username, $password, $dbname);
+
+if($conn->connect_error){
+	die("Connection failed:". $conn->connect_error);
+}
+
+$target=array();
+$data=array();
+//preset word
+$preset='spring';
+$sql=" SELECT keywords, description FROM suggestion WHERE description='$preset' ";
+$result=mysqli_query($conn,$sql);
+$text=mysqli_fetch_assoc($result);
+//store keyword into array
+$text=explode(" ",$text['keywords']);
+$len=count($text);
+//searching product that matches at least one keyword
+if (count($text)>0){
+	$query=" SELECT * FROM product ";
+	$query=mysqli_query($conn,$query);
+	$numrows=mysqli_num_rows($query);
+	if ($numrows>0){
+		while ($row=mysqli_fetch_assoc($query)){
+			$sum=0;
+			$pid=$row['id'];
+			//loop for all key words according to space
+			for ($i=0; $i<$len; $i++){
+				if (stripos($row['name'], $text[$i])!==false){
+					$sum++;
+					continue;
+				}
+				//convert type from int to string
+				if ($row['type']==1){
+	                $type="new";
+                }
+                else{
+	                $type="second hand";
+                }
+			    if (stripos($type, $text[$i])!==false){
+					$sum++;
+					continue;
+				}
+				$info=explode(" ", $row['info']);
+				$key=0;
+				for ($j=0; $j<count($info);$j++){
+					if (stripos($info[$j],$text[$i])!==false){
+						$sum++;
+						$key=1;
+						break;				
+					}	
+				}
+				if ($key==1){
+					continue;
+				}
+				$sql=" SELECT category, pid FROM categories WHERE pid=$pid";
+	            $sql=mysqli_query($conn,$sql);
+	            $caterows=mysqli_num_rows($sql);
+	            if ($caterows>0){
+		            while ($r=mysqli_fetch_assoc($sql)){
+						//convert cate to string
+						switch($r['category']){
+							case 1: 
+		                        $tmp="Textbook and Source";
+			                    break;
+	                        case 2:
+		                        $tmp="Electronics";
+			                    break;
+	                        case 3:
+		                        $tmp="Furnitures";
+			                    break;
+	                        case 4:
+		                        $tmp="Dorm items/ Necessities";
+			                    break;
+	                        case 5:
+		                        $tmp="Clothes";
+			                    break;
+                            case 6:
+		                        $tmp="Stationery";
+			                    break;
+                            case 7:
+		                        $tmp="Others";
+							};
+				        if (stripos($tmp, $text[$i])!==false){
+					        $sum++;
+					        break;
+				        }			            
+		            }
+	            }
+			}
+			if ($sum>0){
+				$pid=$row['id'];
+				array_push($target,$pid);
+				array_push($target,$sum);
+				array_push($data, $target);
+				$target=array();
+			}
+		}		
+	}		
+}
+//quicksort on $data according to appearance times of keywords
+function swap(&$a, &$b) 
+{  
+    $temp = $a;  
+    $a = $b;  
+    $b = $temp;  
+}  
+
+function partition (&$arr, $l, $h)  
+{  
+    $x = $arr[$h][1];  
+    $i = ($l - 1);  
+  
+    for ($j = $l; $j <= $h - 1; $j++)  
+    {  
+        if ($arr[$j][1] <= $x)  
+        {  
+            $i++;  
+            swap ($arr[$i][0], $arr[$j][0]); 
+			swap ($arr[$i][1], $arr[$j][1]); 
+        }  
+    }  
+    swap ($arr[$i + 1][0], $arr[$h][0]);
+	swap ($arr[$i + 1][1], $arr[$h][1]);
+	
+    return ($i + 1);  
+}   
+
+function quickSort(&$A, $l, $h)  
+{  
+    if ($l < $h)  
+    {  
+        $p = partition($A, $l, $h);  
+        quickSort($A, $l, $p - 1);  
+        quickSort($A, $p + 1, $h);  
+    }  
+}
+quickSort($data, 0 , count($data)-1);
+?>
+<?php
+$len=count($data);
+$display=6;
+if ($len<6){
+	$display=$len;
+}
+for ($i=0; $i<$display; $i++){
+    $query="select * from trans where pid=".$data[$len-1-$i][0];
+    $result=mysqli_query($conn,$query);
+    if(mysqli_num_rows($result)>0){
+        continue;
+    }
+    $query="select img from product where id=".$data[$len-1-$i][0];
+    $result=mysqli_query($conn,$query);
+    $row=mysqli_fetch_assoc($result);
+    $img=$row['img'];
+    
+?>
+<div class="column">
+<a onclick="window.top.location.href='/product/buy/buying.php?pid=<?php echo $data[$len-1-$i][0];?>'"><img src="/img/product/<?php echo $img;?>" alt="cart" id="img" style="width:100%" style="height:50%"></a>
+	<?php
+	$qry=" SELECT id, name, price FROM product WHERE id=".$data[$len-$i-1][0];
+        $result=mysqli_query($conn,$qry);
+	$row=mysqli_fetch_assoc($result)
+	?>
+	<div>
+	<?php
+        echo "Itemname: ",$row['name'],"<br>";
+	echo "Price: $",$row['price'];
+	?>
+	</div>
+</div>
+<?php
+}
+mysqli_close($conn);
+?>
 
 <div class="push"></div>
 
